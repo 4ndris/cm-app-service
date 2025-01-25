3123e547b5ae097ae8db9fe5d88d320285d3de28
Andras Vereb <v.4ndras@gmail.com>
2025-01-25 16:51:22 +0100

    fix annotation

--- src/main/java/hu/vereba/cm/database/respository/ShowRepository.java
+++ src/main/java/hu/vereba/cm/database/respository/ShowRepository.java
@@ -1,6 +1,7 @@
 package hu.vereba.cm.database.respository;
 
 import hu.vereba.cm.database.entity.BaseShowEntity;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ import java.util.Optional;
 public interface ShowRepository<T extends BaseShowEntity> extends CrudRepository<T, Long> {
 
     @Override
+    @NotNull
     List<T> findAll();
 
     Optional<T> findById(String id);
